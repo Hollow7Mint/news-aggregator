e1144bd60d865217005fc74acbca5e76d2d169aa
James Holloway <james.holloway94@gmail.com>
2026-04-29 02:47:24 +0000

    resolve known_hosts cleanup sync into metadata

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-04-29 01:56:42 UTC
+# Last sync: 2026-04-29 02:47:24 UTC
