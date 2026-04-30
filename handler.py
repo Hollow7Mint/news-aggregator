5466396287fd2d9341a8464c7e427614e235cf30
James Holloway <james.holloway94@gmail.com>
2026-04-30 16:43:14 +0000

    load mysql slow query log sync from pipeline

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-04-30 15:10:56 UTC
+# Last sync: 2026-04-30 16:43:14 UTC
