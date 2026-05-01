88f1f2386dc9c11b5ca7fec8befcb8decf30e20c
James Holloway <james.holloway94@gmail.com>
2026-05-01 11:08:57 +0000

    relay db connection string time through config

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-01 10:40:47 UTC
+# Last sync: 2026-05-01 11:08:57 UTC
