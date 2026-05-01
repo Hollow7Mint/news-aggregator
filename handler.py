eead18bd70f143b2db65f7dee5e313a66e2b7316
James Holloway <james.holloway94@gmail.com>
2026-05-01 17:18:26 +0000

    catalog mongodb document count sync after audit

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-01 17:01:59 UTC
+# Last sync: 2026-05-01 17:18:26 UTC
