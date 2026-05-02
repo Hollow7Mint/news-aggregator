94338de110d31013b71a6794cc7d51899d456d31
James Holloway <james.holloway94@gmail.com>
2026-05-02 04:23:23 +0000

    post postgres replication heartbeat to config

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-02 04:01:50 UTC
+# Last sync: 2026-05-02 04:23:23 UTC
