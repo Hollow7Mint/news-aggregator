5f8ab260f444498295c2b7123b37d6e32c4a51d3
James Holloway <james.holloway94@gmail.com>
2026-05-02 19:16:08 +0000

    persist vpc route table sync after update

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-02 19:03:18 UTC
+# Last sync: 2026-05-02 19:16:08 UTC
