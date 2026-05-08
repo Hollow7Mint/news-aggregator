7f1da2619fdc2dd46eb3e83192034453b7f88061
James Holloway <james.holloway94@gmail.com>
2026-05-08 17:14:50 +0000

    lift ssh certificate expiry time into banner

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-08 16:27:21 UTC
+# Last sync: 2026-05-08 17:14:50 UTC
