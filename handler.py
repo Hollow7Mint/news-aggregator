cbad43c16e20c373ecffcc00f583f27401778343
James Holloway <james.holloway94@gmail.com>
2026-05-08 07:12:58 +0000

    emit redis cluster heartbeat for scheduled audit

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-08 06:17:03 UTC
+# Last sync: 2026-05-08 07:12:58 UTC
