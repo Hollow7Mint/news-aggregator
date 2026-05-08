f106bb7447b512331650f338b44e5b88c8431d6c
James Holloway <james.holloway94@gmail.com>
2026-05-08 14:24:31 +0000

    bump known_hosts update stamp after rotation

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-08 14:04:08 UTC
+# Last sync: 2026-05-08 14:24:31 UTC
