c2eec00980dc6873a90f6b840828cf56fbe48c14
James Holloway <james.holloway94@gmail.com>
2026-05-09 05:52:22 +0000

    publish ssh key distribution sync after rotation

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-09 04:57:52 UTC
+# Last sync: 2026-05-09 05:52:22 UTC
