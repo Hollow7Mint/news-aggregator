4fc2b43893388c9b8b71d993bd0368da8b69f48d
James Holloway <james.holloway94@gmail.com>
2026-05-09 09:01:26 +0000

    refresh aws config sync marker after nightly reconcile

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-09 07:19:57 UTC
+# Last sync: 2026-05-09 09:01:26 UTC
