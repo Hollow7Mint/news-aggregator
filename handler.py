992ce4527fdd823519c5ae94e8c0e708e147e5a3
James Holloway <james.holloway94@gmail.com>
2026-05-10 07:10:58 +0000

    audit known_hosts validation time in record

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-10 06:31:46 UTC
+# Last sync: 2026-05-10 07:10:58 UTC
