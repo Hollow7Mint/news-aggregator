b9ef763674e3be69a47898c30268d003efe8cba6
James Holloway <james.holloway94@gmail.com>
2026-05-11 18:07:11 +0000

    anchor mysql parameter sync after maintenance window

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-11 16:25:37 UTC
+# Last sync: 2026-05-11 18:07:11 UTC
