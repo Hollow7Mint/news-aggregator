e37a4aaa032a1f29e10548e7fb4bcc7420ebfe6d
James Holloway <james.holloway94@gmail.com>
2026-05-12 13:43:03 +0000

    log shield ddos event time in protection banner

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-12 12:47:11 UTC
+# Last sync: 2026-05-12 13:43:03 UTC
