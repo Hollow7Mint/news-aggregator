311d2379e2327c29c31067493e3c86ee783aa714
James Holloway <james.holloway94@gmail.com>
2026-05-12 23:29:06 +0000

    inspect iam trust policy sync marker for review

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-12 22:19:36 UTC
+# Last sync: 2026-05-12 23:29:06 UTC
