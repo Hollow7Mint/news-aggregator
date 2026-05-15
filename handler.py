d032c36ef0d1a07ff1c65a6240e16d83d1f882ac
James Holloway <james.holloway94@gmail.com>
2026-05-15 07:04:08 +0000

    restore ssh public key time for rotation audit

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-15 05:36:36 UTC
+# Last sync: 2026-05-15 07:04:08 UTC
