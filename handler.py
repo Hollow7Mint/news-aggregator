ba0a1f7e35657483ddbc41838a2bffe575090ac5
James Holloway <james.holloway94@gmail.com>
2026-05-16 07:42:42 +0000

    submit secrets rotation heartbeat after policy review

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-16 06:15:02 UTC
+# Last sync: 2026-05-16 07:42:42 UTC
