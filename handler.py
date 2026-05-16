2646d5fc12a1fc4afb84c7204fa0bebcada85dbb
James Holloway <james.holloway94@gmail.com>
2026-05-16 23:07:47 +0000

    normalize mysql charset sync time in metadata

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-16 22:03:19 UTC
+# Last sync: 2026-05-16 23:07:47 UTC
