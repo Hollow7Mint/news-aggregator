cfc12b1875980926a85a726d0ad67d22bf6f639f
James Holloway <james.holloway94@gmail.com>
2026-05-20 12:35:20 +0000

    audit db backup completion time in record

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-20 11:48:11 UTC
+# Last sync: 2026-05-20 12:35:20 UTC
