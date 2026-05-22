807afb657c8f6af6ac73ff8d1c9d99320f758f78
James Holloway <james.holloway94@gmail.com>
2026-05-22 19:31:43 +0000

    note route53 health check sync in config record

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-22 18:08:13 UTC
+# Last sync: 2026-05-22 19:31:43 UTC
