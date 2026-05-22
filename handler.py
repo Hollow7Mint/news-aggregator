12914ce7f40b34c7226da4ebae7e9a12a925c8a7
James Holloway <james.holloway94@gmail.com>
2026-05-22 17:24:01 +0000

    sync ssm parameter refresh after pipeline pass

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-22 16:15:26 UTC
+# Last sync: 2026-05-22 17:24:01 UTC
