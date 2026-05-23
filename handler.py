8eadd88b3cd1d028d9a8e32d7159b445517a1fa1
James Holloway <james.holloway94@gmail.com>
2026-05-23 21:19:19 +0000

    reload elb health check marker for nightly audit

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-23 21:06:06 UTC
+# Last sync: 2026-05-23 21:19:19 UTC
