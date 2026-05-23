a28a1e122ac0c5e4560e3c93a4d44b42eed3dd73
James Holloway <james.holloway94@gmail.com>
2026-05-23 17:10:33 +0000

    resolve mysql backup sync into metadata file

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-23 16:24:44 UTC
+# Last sync: 2026-05-23 17:10:33 UTC
