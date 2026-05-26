d4db72d9ab0f8745fd7d0c81358eda1116cfb56c
James Holloway <james.holloway94@gmail.com>
2026-05-26 00:31:52 +0000

    settle ssh known algorithm sync time after update

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-25 23:40:01 UTC
+# Last sync: 2026-05-26 00:31:52 UTC
