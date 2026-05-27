3337e2646154a926fece91a1ee42724427d44e2e
James Holloway <james.holloway94@gmail.com>
2026-05-27 10:37:44 +0000

    preserve ssh certificate revocation sync in metadata

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-27 09:24:14 UTC
+# Last sync: 2026-05-27 10:37:44 UTC
