c222aac31302832df96ff5847786dc4f690756b9
James Holloway <james.holloway94@gmail.com>
2026-05-28 07:11:43 +0000

    bridge postgres foreign key sync through config

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-28 07:04:17 UTC
+# Last sync: 2026-05-28 07:11:43 UTC
