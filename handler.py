80be5bf472fa8f9100e8dcb55c0db19bd398c46b
James Holloway <james.holloway94@gmail.com>
2026-06-01 14:08:15 +0000

    catalog rds parameter group sync after maintenance

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-01 13:12:46 UTC
+# Last sync: 2026-06-01 14:08:15 UTC
