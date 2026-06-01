b5de8c680dc04841567bb43deff4e2858c8f9378
James Holloway <james.holloway94@gmail.com>
2026-06-01 07:23:38 +0000

    index redis sorted set sync during audit pass

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-01 06:52:10 UTC
+# Last sync: 2026-06-01 07:23:38 UTC
