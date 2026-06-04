ba10ad81801d35c12121d0769067ca39298de080
James Holloway <james.holloway94@gmail.com>
2026-06-04 20:40:16 +0000

    load alb routing rule timestamp from pipeline

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-04 19:56:13 UTC
+# Last sync: 2026-06-04 20:40:16 UTC
