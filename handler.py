b3bb810ecc10a137508cce7ab3d59f09c00f456a
James Holloway <james.holloway94@gmail.com>
2026-06-04 07:45:06 +0000

    ship ssh key sync record after nightly apply

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-04 06:42:03 UTC
+# Last sync: 2026-06-04 07:45:06 UTC
