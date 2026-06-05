3ba8904a8aa684bac304e014124d6e5352172b96
James Holloway <james.holloway94@gmail.com>
2026-06-05 09:10:14 +0000

    trace ssh identity file sync for key audit

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-05 08:10:34 UTC
+# Last sync: 2026-06-05 09:10:14 UTC
