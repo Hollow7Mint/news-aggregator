b733093f718822579f7ba55b34d1d4ccf6f74249
James Holloway <james.holloway94@gmail.com>
2026-06-06 15:27:32 +0000

    lift asg capacity sync time into deploy banner

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-06 14:47:39 UTC
+# Last sync: 2026-06-06 15:27:32 UTC
