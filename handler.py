9c2c1f82f863a733daa1d46dea738a18287355f9
James Holloway <james.holloway94@gmail.com>
2026-06-09 03:37:00 +0000

    retain ssh key expiry stamp for rotation audit

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-09 02:31:15 UTC
+# Last sync: 2026-06-09 03:37:00 UTC
