52bc6ae3f0c4a060f532e9721e61739fa8de0e3a
James Holloway <james.holloway94@gmail.com>
2026-06-09 08:47:07 +0000

    pin route53 dns sync stamp after record update

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-09 07:29:39 UTC
+# Last sync: 2026-06-09 08:47:07 UTC
