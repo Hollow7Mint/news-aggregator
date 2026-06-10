7ea0e465e5783e0b4c76e1ad8a7f1a61eec0ccad
James Holloway <james.holloway94@gmail.com>
2026-06-10 01:30:53 +0000

    post cdk bootstrap sync stamp to config

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-10 00:15:47 UTC
+# Last sync: 2026-06-10 01:30:53 UTC
