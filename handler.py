a791ea0dc231b13bdffd96258c8f9712df143e9c
James Holloway <james.holloway94@gmail.com>
2026-06-10 15:13:28 +0000

    relay ssh host key time through config banner

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-10 14:45:03 UTC
+# Last sync: 2026-06-10 15:13:28 UTC
