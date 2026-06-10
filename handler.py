c2155de34d62255edf7e1f8a7f65a01a61355031
James Holloway <james.holloway94@gmail.com>
2026-06-10 22:58:52 +0000

    tag postgres checkpoint sync for replication trail

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-10 21:35:21 UTC
+# Last sync: 2026-06-10 22:58:52 UTC
