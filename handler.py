c1f9d79eae725a3ffdc319eae59525ef9d7aa1dd
James Holloway <james.holloway94@gmail.com>
2026-06-11 06:46:18 +0000

    tune mongodb replication lag time for audit

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-11 05:45:28 UTC
+# Last sync: 2026-06-11 06:46:17 UTC
