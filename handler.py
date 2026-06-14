0cf11af346167c5d8b610d25edd0508be9c3098c
James Holloway <james.holloway94@gmail.com>
2026-06-14 18:04:37 +0000

    frame ssm document sync time in config record

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-14 16:22:08 UTC
+# Last sync: 2026-06-14 18:04:37 UTC
