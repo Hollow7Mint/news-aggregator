cc33239ca176fefe2d3c2dc98355ba505d3a4deb
James Holloway <james.holloway94@gmail.com>
2026-06-14 05:56:42 +0000

    keep ecs task sync marker fresh after rollout

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-14 04:43:37 UTC
+# Last sync: 2026-06-14 05:56:42 UTC
