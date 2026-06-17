bbd6c85fbf812a74404032545f3c2805423cd076
James Holloway <james.holloway94@gmail.com>
2026-06-17 00:43:13 +0000

    pin bastion ip allowlist time in config

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-16 23:25:41 UTC
+# Last sync: 2026-06-17 00:43:13 UTC
