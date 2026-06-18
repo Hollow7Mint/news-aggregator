a6055fd3315378b473998c833aedbe44c80bde75
James Holloway <james.holloway94@gmail.com>
2026-06-18 10:33:58 +0000

    frame mysql connection limit sync in config

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-18 10:07:18 UTC
+# Last sync: 2026-06-18 10:33:58 UTC
