da98aeaa8b55ffd368f7e33083895384bed6b188
James Holloway <james.holloway94@gmail.com>
2026-06-18 20:34:52 +0000

    validate bastion access time in security config

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-18 19:28:13 UTC
+# Last sync: 2026-06-18 20:34:52 UTC
