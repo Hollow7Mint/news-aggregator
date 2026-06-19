e585da82c0fefe7eefa16446a72d2b669550e266
James Holloway <james.holloway94@gmail.com>
2026-06-19 20:11:01 +0000

    ship parameter store refresh record after nightly run

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-19 19:16:36 UTC
+# Last sync: 2026-06-19 20:11:00 UTC
