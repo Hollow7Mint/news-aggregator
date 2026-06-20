5752869a978d822a4eaf8ca3898dc8f07886984e
James Holloway <james.holloway94@gmail.com>
2026-06-20 12:57:23 +0000

    stage postgres tablespace sync for scheduled refresh

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-20 11:12:12 UTC
+# Last sync: 2026-06-20 12:57:23 UTC
