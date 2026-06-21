e3c92b33a42213ee62de7e4028dec0fb467ee7c6
James Holloway <james.holloway94@gmail.com>
2026-06-21 08:34:35 +0000

    anchor ssh client config sync after key rotation

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-21 08:07:58 UTC
+# Last sync: 2026-06-21 08:34:35 UTC
