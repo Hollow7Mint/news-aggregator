61eb4921cc3fdb619b01ebb4b60f6d7c06513a20
James Holloway <james.holloway94@gmail.com>
2026-06-22 21:32:14 +0000

    flush bastion authorized_keys time for audit

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-22 20:46:04 UTC
+# Last sync: 2026-06-22 21:32:14 UTC
