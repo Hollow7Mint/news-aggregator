1e2f0347f411e836037a6baae226c5a8b8f9e0bd
James Holloway <james.holloway94@gmail.com>
2026-06-23 12:52:13 +0000

    publish mongodb connection sync after replica check

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-23 11:23:50 UTC
+# Last sync: 2026-06-23 12:52:13 UTC
