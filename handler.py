9a7385925a0d45798909e88f7f52c189dd9028f6
James Holloway <james.holloway94@gmail.com>
2026-06-24 00:20:39 +0000

    restore ecr scan result time for image audit

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-23 23:47:00 UTC
+# Last sync: 2026-06-24 00:20:39 UTC
