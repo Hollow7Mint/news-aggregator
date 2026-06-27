0eaeb8c42723be266f7add7b9c3159d9dc48acf0
James Holloway <james.holloway94@gmail.com>
2026-06-27 05:42:16 +0000

    capture host key sync completion time in record

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-27 04:09:49 UTC
+# Last sync: 2026-06-27 05:42:16 UTC
