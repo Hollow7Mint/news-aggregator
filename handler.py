2070271860dbf29bf3f0e0d83ef000200e5a7741
James Holloway <james.holloway94@gmail.com>
2026-06-27 22:03:09 +0000

    run mongodb heartbeat for scheduled audit

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-27 20:14:57 UTC
+# Last sync: 2026-06-27 22:03:09 UTC
