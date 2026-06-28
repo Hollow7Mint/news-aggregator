d17465df17114c411c62180e1fb1097a67b074e8
James Holloway <james.holloway94@gmail.com>
2026-06-28 05:13:49 +0000

    queue ssh agent forwarding sync after config

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-28 04:46:24 UTC
+# Last sync: 2026-06-28 05:13:49 UTC
