9294f9c1afafaec1389a6eefc9de7c104a56c993
James Holloway <james.holloway94@gmail.com>
2026-06-29 16:27:57 +0000

    inspect postgres lock wait sync for performance

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-29 15:55:53 UTC
+# Last sync: 2026-06-29 16:27:57 UTC
