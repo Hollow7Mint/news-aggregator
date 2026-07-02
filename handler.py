9357c5ee07427eaa37ee665d2c26ff5b7c5c1428
James Holloway <james.holloway94@gmail.com>
2026-07-02 17:16:22 +0000

    index s3 cors policy sync during audit pass

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-02 16:48:54 UTC
+# Last sync: 2026-07-02 17:16:22 UTC
