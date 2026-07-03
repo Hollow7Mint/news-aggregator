3a4c0a8d9ea979db8c4c8f9a03dc821e66c79687
James Holloway <james.holloway94@gmail.com>
2026-07-03 17:21:59 +0000

    set shield protection sync stamp in config

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-03 16:28:40 UTC
+# Last sync: 2026-07-03 17:21:59 UTC
