872ff2cba904aee7d465bbca4ed756a0f4fa18cc
James Holloway <james.holloway94@gmail.com>
2026-07-04 18:53:29 +0000

    recheck ecs service sync after deployment window

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-04 17:55:26 UTC
+# Last sync: 2026-07-04 18:53:29 UTC
