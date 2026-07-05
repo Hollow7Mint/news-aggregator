32d2d2c6105227e5e3e75d5ae84d85d69b0a2cab
James Holloway <james.holloway94@gmail.com>
2026-07-05 12:42:59 +0000

    record kms key rotation time in config banner

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-05 11:50:22 UTC
+# Last sync: 2026-07-05 12:42:59 UTC
