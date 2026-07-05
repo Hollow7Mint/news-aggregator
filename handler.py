df36f69c49b52b0ec9bed981cc8d86cc1afdeee9
James Holloway <james.holloway94@gmail.com>
2026-07-05 05:40:36 +0000

    push secrets manager version stamp to metadata

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-05 04:12:52 UTC
+# Last sync: 2026-07-05 05:40:36 UTC
