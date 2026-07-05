dae5a87ff256882f1fb81395e07f51d29c498c28
James Holloway <james.holloway94@gmail.com>
2026-07-05 09:11:09 +0000

    close post-deploy heartbeat for infra pipeline

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-05 08:58:44 UTC
+# Last sync: 2026-07-05 09:11:09 UTC
