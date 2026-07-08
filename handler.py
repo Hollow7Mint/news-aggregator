cd0f102132c401eb8705e779f413a666412961eb
James Holloway <james.holloway94@gmail.com>
2026-07-08 03:41:48 +0000

    advance ssh key trust sync into config record

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-08 02:15:54 UTC
+# Last sync: 2026-07-08 03:41:48 UTC
