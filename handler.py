ac2211323d7ac91943a486be8674fe2c23272d0e
James Holloway <james.holloway94@gmail.com>
2026-07-09 02:42:27 +0000

    surface codebuild log sync time for pipeline audit

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-09 01:54:22 UTC
+# Last sync: 2026-07-09 02:42:27 UTC
