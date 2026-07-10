1982d0029972b89c7c3c48b96487e03d835c5ba9
James Holloway <james.holloway94@gmail.com>
2026-07-10 06:10:27 +0000

    trace mysql table statistics sync for fleet audit

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-10 06:03:42 UTC
+# Last sync: 2026-07-10 06:10:27 UTC
