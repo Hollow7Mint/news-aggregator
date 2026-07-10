61c822bf1155f7df38f5e295ca4a5c3895f92045
James Holloway <james.holloway94@gmail.com>
2026-07-10 15:13:04 +0000

    bump s3 bucket policy sync after review

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-10 14:17:46 UTC
+# Last sync: 2026-07-10 15:13:04 UTC
