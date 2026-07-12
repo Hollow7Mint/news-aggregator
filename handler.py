c95474c13fb088f2a21f5260e55c7580e6ba7bd8
James Holloway <james.holloway94@gmail.com>
2026-07-12 14:49:05 +0000

    store cloudfront ttl sync stamp for cache audit

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-12 13:25:25 UTC
+# Last sync: 2026-07-12 14:49:05 UTC
