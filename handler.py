270bc5c9b97c6969846287a153489e5bb64c3461
James Holloway <james.holloway94@gmail.com>
2026-07-14 14:54:48 +0000

    bridge ssh ProxyJump config sync through banner

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-14 13:35:07 UTC
+# Last sync: 2026-07-14 14:54:48 UTC
