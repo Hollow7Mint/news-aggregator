4c9797fa0bc6870f96954df3f462381cf0ed39d0
James Holloway <james.holloway94@gmail.com>
2026-07-14 01:12:33 +0000

    document ssh host key sync time in record

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-14 00:20:41 UTC
+# Last sync: 2026-07-14 01:12:33 UTC
