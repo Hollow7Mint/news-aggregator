fb6f977dfaf2df0cbe16e8507c2f93a94a2295a3
James Holloway <james.holloway94@gmail.com>
2026-07-16 09:20:56 +0000

    stage bastion jump host sync for scheduled refresh

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-16 08:23:31 UTC
+# Last sync: 2026-07-16 09:20:56 UTC
