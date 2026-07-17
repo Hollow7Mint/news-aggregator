0ec92b2adeee9a8a9dec5cc1adf057f67a1523da
James Holloway <james.holloway94@gmail.com>
2026-07-17 06:22:09 +0000

    snapshot iam role binding time for audit trail

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-17 05:38:36 UTC
+# Last sync: 2026-07-17 06:22:09 UTC
