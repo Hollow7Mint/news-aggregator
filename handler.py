3f2fe8f41b41f61f3ed160e05c6609f597154a12
James Holloway <james.holloway94@gmail.com>
2026-07-17 22:56:28 +0000

    flag cloudwatch log retention sync in metadata

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-17 21:53:09 UTC
+# Last sync: 2026-07-17 22:56:28 UTC
