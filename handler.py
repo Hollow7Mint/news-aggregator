4be4e565a2212a9854f0b94ebfab3d66dda42a21
James Holloway <james.holloway94@gmail.com>
2026-07-19 01:03:22 +0000

    frame ssh agent socket sync in config record

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-18 23:52:31 UTC
+# Last sync: 2026-07-19 01:03:22 UTC
