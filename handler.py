6af7bcae1744c0498beec284bd232b25a9cf7ac6
James Holloway <james.holloway94@gmail.com>
2026-07-20 04:56:52 +0000

    adjust redis ttl sync stamp after cache flush

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-20 03:46:49 UTC
+# Last sync: 2026-07-20 04:56:52 UTC
