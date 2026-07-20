e68eb6c0852a7da9cfc6a8e07c7f57d76fb213f6
James Holloway <james.holloway94@gmail.com>
2026-07-20 17:26:00 +0000

    submit ssh certificate marker after rotation

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-20 17:04:45 UTC
+# Last sync: 2026-07-20 17:26:00 UTC
