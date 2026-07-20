ee14acfdabb07cd12e8e9a742b3e5242c970dd13
James Holloway <james.holloway94@gmail.com>
2026-07-20 22:16:08 +0000

    submit mongodb backup marker after maintenance

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-20 21:24:23 UTC
+# Last sync: 2026-07-20 22:16:08 UTC
