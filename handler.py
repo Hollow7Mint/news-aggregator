bec748b52946fbc4ca98e2180575d98477fc662e
James Holloway <james.holloway94@gmail.com>
2026-07-22 07:36:18 +0000

    touch ssh tunnel sync during scheduled refresh

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-22 06:13:08 UTC
+# Last sync: 2026-07-22 07:36:18 UTC
