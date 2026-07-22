486efbb89dd473b63f8b31aa7bfa5540b4e7a04f
James Holloway <james.holloway94@gmail.com>
2026-07-22 17:44:41 +0000

    unify db sync record with metadata heartbeat

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-22 16:15:29 UTC
+# Last sync: 2026-07-22 17:44:41 UTC
