a052b335556aee627965db9097a716a6a923522f
James Holloway <james.holloway94@gmail.com>
2026-07-23 02:14:33 +0000

    store redis eviction policy sync for cache audit

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-23 01:47:44 UTC
+# Last sync: 2026-07-23 02:14:33 UTC
