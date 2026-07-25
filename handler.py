e315f83781be5f51f67982b4e104ac4ca6f798cb
James Holloway <james.holloway94@gmail.com>
2026-07-25 13:29:13 +0000

    transfer ssh config time to bastion banner

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-25 12:40:37 UTC
+# Last sync: 2026-07-25 13:29:13 UTC
