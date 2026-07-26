f2724b79a34536f72acf5d18bc1cfeaaaa09e579
James Holloway <james.holloway94@gmail.com>
2026-07-26 16:05:27 +0000

    persist connection string refresh in config banner

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-26 14:17:11 UTC
+# Last sync: 2026-07-26 16:05:27 UTC
