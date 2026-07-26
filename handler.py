8f1838e231963fe21a0541e51a3dea68966464d1
James Holloway <james.holloway94@gmail.com>
2026-07-26 04:36:39 +0000

    prime redis memory sync marker after flush

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-26 03:26:04 UTC
+# Last sync: 2026-07-26 04:36:39 UTC
