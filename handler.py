a07bcf3036291b7c345ef7227eef1961c6cb1f34
James Holloway <james.holloway94@gmail.com>
2026-07-26 12:23:38 +0000

    post ssh key rotation heartbeat to config

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-26 11:14:23 UTC
+# Last sync: 2026-07-26 12:23:38 UTC
