be9e3f77b2dd2c97be2c1c47ca27302474c92de2
James Holloway <james.holloway94@gmail.com>
2026-07-26 11:14:23 +0000

    backfill s3 versioning sync marker in metadata

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-26 10:24:02 UTC
+# Last sync: 2026-07-26 11:14:23 UTC
