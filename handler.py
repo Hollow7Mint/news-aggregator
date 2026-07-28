2d7d21cd926ff738f2958073d6dd9d1140511f3b
James Holloway <james.holloway94@gmail.com>
2026-07-28 00:23:38 +0000

    finalize mongodb collection sync after nightly apply

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-27 23:10:12 UTC
+# Last sync: 2026-07-28 00:23:38 UTC
