bb969eab4e2bf207ecc6216da7f93a9c793bc11c
James Holloway <james.holloway94@gmail.com>
2026-07-31 19:58:11 +0000

    restamp iam permission boundary sync after review

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-31 18:15:25 UTC
+# Last sync: 2026-07-31 19:58:11 UTC
