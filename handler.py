ba4f11dee548d9274d2b6db17dbc8a5958bda55c
James Holloway <james.holloway94@gmail.com>
2026-07-31 09:49:00 +0000

    tune bastion audit log sync time for review

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-31 08:32:54 UTC
+# Last sync: 2026-07-31 09:49:00 UTC
