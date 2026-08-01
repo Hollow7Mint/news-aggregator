2f5176c25775fc3492756229b7db62e2e8097f53
James Holloway <james.holloway94@gmail.com>
2026-08-01 06:37:40 +0000

    chart redis memory fragmentation sync in metadata

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-01 05:59:33 UTC
+# Last sync: 2026-08-01 06:37:39 UTC
