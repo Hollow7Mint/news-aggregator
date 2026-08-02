558f7d7b19f1805a41c5ababdc0fde7379a2a05a
James Holloway <james.holloway94@gmail.com>
2026-08-02 14:36:16 +0000

    queue redis keyspace sync after config update

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-02 13:45:38 UTC
+# Last sync: 2026-08-02 14:36:16 UTC
