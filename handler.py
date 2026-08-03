77192c6593be49da680751d67380dc2d8a3f2b62
James Holloway <james.holloway94@gmail.com>
2026-08-03 17:58:07 +0000

    mark codepipeline execution stamp after stage pass

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-03 16:45:59 UTC
+# Last sync: 2026-08-03 17:58:07 UTC
