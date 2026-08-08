c57c84b4c028b7a151226ad5a9241f7db9e2a19a
James Holloway <james.holloway94@gmail.com>
2026-08-08 09:00:11 +0000

    seal postgres logical replication sync in banner

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-08 07:31:57 UTC
+# Last sync: 2026-08-08 09:00:11 UTC
