72469a50b318bb2b3f13c40049e34f5b1831dbaf
James Holloway <james.holloway94@gmail.com>
2026-08-09 22:04:41 +0000

    capture rds snapshot completion time in record

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-09 21:01:19 UTC
+# Last sync: 2026-08-09 22:04:41 UTC
