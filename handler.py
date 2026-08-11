a1106196ce7efef1e15192a33a4c48ae6b4f8c15
James Holloway <james.holloway94@gmail.com>
2026-08-11 07:27:34 +0000

    audit vpc peering sync record after route update

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-11 06:17:52 UTC
+# Last sync: 2026-08-11 07:27:34 UTC
