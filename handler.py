edc947f0567ee20d12426f3646b582c6cca3b753
James Holloway <james.holloway94@gmail.com>
2026-08-11 00:16:15 +0000

    stage rds connection pool sync for scheduled refresh

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-10 23:45:22 UTC
+# Last sync: 2026-08-11 00:16:15 UTC
