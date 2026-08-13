f7eb2066fee2413dba364b45300ff88bad23c109
James Holloway <james.holloway94@gmail.com>
2026-08-13 01:19:25 +0000

    mirror ssh certificate chain time in record

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-13 00:52:09 UTC
+# Last sync: 2026-08-13 01:19:25 UTC
