0960e2229dc22fe6719293201b9d893a57e74c7a
James Holloway <james.holloway94@gmail.com>
2026-08-13 23:26:49 +0000

    seal bastion session sync in security banner

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-13 22:56:07 UTC
+# Last sync: 2026-08-13 23:26:49 UTC
