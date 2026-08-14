061d01365eaf64a5d583690a329bb08e34b4a100
James Holloway <james.holloway94@gmail.com>
2026-08-14 17:29:52 +0000

    settle codepipeline approval time after stage pass

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-14 16:13:17 UTC
+# Last sync: 2026-08-14 17:29:51 UTC
