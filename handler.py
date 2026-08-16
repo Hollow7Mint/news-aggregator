b4987532cfa97e17df560dba37a5aa7d90e42105
James Holloway <james.holloway94@gmail.com>
2026-08-16 15:18:52 +0000

    push ssh config sync marker after host refresh

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-16 14:35:02 UTC
+# Last sync: 2026-08-16 15:18:52 UTC
