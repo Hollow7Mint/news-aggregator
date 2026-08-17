8c95d3b9d69f05003b9dc75842a17b5d384d45b9
James Holloway <james.holloway94@gmail.com>
2026-08-17 09:06:59 +0000

    chart ssh connection attempt sync in metadata

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-17 07:32:20 UTC
+# Last sync: 2026-08-17 09:06:59 UTC
