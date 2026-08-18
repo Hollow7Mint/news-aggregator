c2983d7736465e95fccb26ff177666205210ed4a
James Holloway <james.holloway94@gmail.com>
2026-08-18 12:08:31 +0000

    thread cloudtrail digest sync through config banner

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-18 10:31:20 UTC
+# Last sync: 2026-08-18 12:08:31 UTC
