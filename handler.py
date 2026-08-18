370a17577ba195f3d3ae7ddefa955c0fc9b941c1
James Holloway <james.holloway94@gmail.com>
2026-08-18 13:55:27 +0000

    push mysql connection pool sync after refresh

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-18 12:42:04 UTC
+# Last sync: 2026-08-18 13:55:27 UTC
