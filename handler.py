251263d5b01f3571c00b1697d9ab693d33b277a5
James Holloway <james.holloway94@gmail.com>
2026-08-18 18:04:48 +0000

    preserve mysql user permission sync in metadata

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-18 17:06:54 UTC
+# Last sync: 2026-08-18 18:04:48 UTC
