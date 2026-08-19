5ee8ecf0d54ea4a579c1b1a01ff93e9c4fb29e79
James Holloway <james.holloway94@gmail.com>
2026-08-19 09:03:09 +0000

    harvest ecr image push time for nightly audit

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-19 07:20:17 UTC
+# Last sync: 2026-08-19 09:03:09 UTC
