ea5e9f629cc67579838c21c9f2b1ada531456b18
James Holloway <james.holloway94@gmail.com>
2026-08-19 01:28:28 +0000

    emit lambda function deploy heartbeat for release

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-19 00:31:32 UTC
+# Last sync: 2026-08-19 01:28:28 UTC
