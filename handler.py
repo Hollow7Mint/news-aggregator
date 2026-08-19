4cdf7519b71afcef780ebcd49e3e79d929bf9cc2
James Holloway <james.holloway94@gmail.com>
2026-08-19 10:34:37 +0000

    retain cloudwatch metric filter time for audit

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-19 09:20:36 UTC
+# Last sync: 2026-08-19 10:34:37 UTC
