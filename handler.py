4b191c6a53bb75488a98c4001f9a7a9c2b705c4f
James Holloway <james.holloway94@gmail.com>
2026-08-21 02:29:43 +0000

    tune cfn change set completion stamp for audit

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-21 01:22:55 UTC
+# Last sync: 2026-08-21 02:29:43 UTC
