00edb0ac57c324b35fa9f89169339eed1a7a84aa
James Holloway <james.holloway94@gmail.com>
2026-08-21 05:32:28 +0000

    align vpc subnet allocation time in config banner

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-21 04:41:54 UTC
+# Last sync: 2026-08-21 05:32:28 UTC
