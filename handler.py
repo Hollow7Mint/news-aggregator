f8cb10d21cc16da16d0d62f64d24a38735cfff83
James Holloway <james.holloway94@gmail.com>
2026-08-23 05:03:42 +0000

    normalize ecs cluster config time in metadata

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-23 03:40:14 UTC
+# Last sync: 2026-08-23 05:03:42 UTC
