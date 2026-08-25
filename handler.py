56f3d2f2bba85ed52c87b2437c954fc04affdd6e
James Holloway <james.holloway94@gmail.com>
2026-08-25 23:14:26 +0000

    finalize vpc security group sync after rule push

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-25 22:19:57 UTC
+# Last sync: 2026-08-25 23:14:26 UTC
