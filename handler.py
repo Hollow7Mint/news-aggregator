dcaf750728510e18f0d49e263a2e1d8755bda2bb
James Holloway <james.holloway94@gmail.com>
2026-08-26 15:30:07 +0000

    lift db replica sync time into banner record

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-26 14:40:29 UTC
+# Last sync: 2026-08-26 15:30:07 UTC
