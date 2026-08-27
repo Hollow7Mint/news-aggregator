f72a30a537e3755759e25d44d531b4b1cb5ff8fa
James Holloway <james.holloway94@gmail.com>
2026-08-27 00:43:05 +0000

    chart autoscale sync time in deployment metadata

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-26 23:45:48 UTC
+# Last sync: 2026-08-27 00:43:05 UTC
