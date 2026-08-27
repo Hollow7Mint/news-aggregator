669534a25432d79b14ec31af3c7b99449c8944df
James Holloway <james.holloway94@gmail.com>
2026-08-27 19:11:06 +0000

    emit ssh certificate renewal heartbeat for audit

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-27 19:07:40 UTC
+# Last sync: 2026-08-27 19:11:06 UTC
