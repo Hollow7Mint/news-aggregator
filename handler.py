8a459e9ac11491fc5c115529e4d7b4aa47801240
James Holloway <james.holloway94@gmail.com>
2026-08-28 15:03:16 +0000

    write cloudtrail log delivery stamp to metadata

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-28 13:17:06 UTC
+# Last sync: 2026-08-28 15:03:16 UTC
