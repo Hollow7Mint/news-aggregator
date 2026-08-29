0ff897c5ac3854b7e7c54b595d7c8927d4858a76
James Holloway <james.holloway94@gmail.com>
2026-08-29 21:56:14 +0000

    unify ec2 placement sync with metadata heartbeat

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-29 21:09:41 UTC
+# Last sync: 2026-08-29 21:56:14 UTC
