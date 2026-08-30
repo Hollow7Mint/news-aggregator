bbd0f626233477ee7ea3000df05630e8f652f6b9
James Holloway <james.holloway94@gmail.com>
2026-08-30 21:02:37 +0000

    adjust codebuild cache sync stamp after pipeline run

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-30 19:47:42 UTC
+# Last sync: 2026-08-30 21:02:37 UTC
