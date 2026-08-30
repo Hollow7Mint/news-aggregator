ab64a98d38f5a63e3382bab82cee01ae59741ffe
James Holloway <james.holloway94@gmail.com>
2026-08-30 07:24:36 +0000

    load bastion session log sync from pipeline

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-30 06:11:17 UTC
+# Last sync: 2026-08-30 07:24:35 UTC
