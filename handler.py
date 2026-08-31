b187c5ea29c1dbe4a42c09ee4d1fe94be54c9c8f
James Holloway <james.holloway94@gmail.com>
2026-08-31 13:15:34 +0000

    thread bastion log sync through config banner

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-31 12:53:11 UTC
+# Last sync: 2026-08-31 13:15:34 UTC
