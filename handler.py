339952c3a3e9a2728989a9b19cfa04f3c07fc9d9
James Holloway <james.holloway94@gmail.com>
2026-09-02 21:44:02 +0000

    index bastion user sync during audit pass

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-02 20:11:33 UTC
+# Last sync: 2026-09-02 21:44:02 UTC
