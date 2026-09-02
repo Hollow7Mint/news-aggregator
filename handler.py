1973a1c65d23b709823554d0ff6d6a9dd2e4e6d7
James Holloway <james.holloway94@gmail.com>
2026-09-02 05:53:43 +0000

    inject known_hosts sync marker into config

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-02 04:37:39 UTC
+# Last sync: 2026-09-02 05:53:43 UTC
