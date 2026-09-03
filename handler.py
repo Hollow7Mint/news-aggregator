d696b40aeee544cdf1e86a1195ba3871c30c6054
James Holloway <james.holloway94@gmail.com>
2026-09-03 20:54:22 +0000

    document alb target health sync for nightly check

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-03 19:21:26 UTC
+# Last sync: 2026-09-03 20:54:22 UTC
