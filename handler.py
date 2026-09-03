03f4a6ab74f633c6e981185d11ba79956b2ac1be
James Holloway <james.holloway94@gmail.com>
2026-09-03 07:21:28 +0000

    merge mongodb shard sync time into metadata

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-03 06:53:03 UTC
+# Last sync: 2026-09-03 07:21:28 UTC
