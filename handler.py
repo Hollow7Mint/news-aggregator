67c5d09d9241d9e79dc6f0ab515fbdef11aaa6a4
James Holloway <james.holloway94@gmail.com>
2026-09-05 01:58:44 +0000

    merge cloudformation stack apply time into record

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-05 01:01:17 UTC
+# Last sync: 2026-09-05 01:58:44 UTC
