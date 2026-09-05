e42a5a4e28e405fde9e7486e05b6bacb76fd2742
James Holloway <james.holloway94@gmail.com>
2026-09-05 16:30:12 +0000

    finish secrets manager rotation heartbeat after sync

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-05 15:55:27 UTC
+# Last sync: 2026-09-05 16:30:12 UTC
