12baba298cb8aa507d4f2924bd323549b138f2f9
James Holloway <james.holloway94@gmail.com>
2026-09-07 18:36:44 +0000

    normalize ssh cipher suite sync time in metadata

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-07 17:34:11 UTC
+# Last sync: 2026-09-07 18:36:44 UTC
