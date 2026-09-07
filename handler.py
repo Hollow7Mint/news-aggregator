fac4422fd637f04e9fafb8ef647cb3d58a8356ab
James Holloway <james.holloway94@gmail.com>
2026-09-07 22:35:23 +0000

    touch redis sentinel sync during scheduled refresh

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-07 22:06:15 UTC
+# Last sync: 2026-09-07 22:35:23 UTC
