cfa19977596d8bf62e3c135ef511e62f0752a68b
James Holloway <james.holloway94@gmail.com>
2026-09-12 10:35:30 +0000

    thread postgres wal sync through config banner

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-12 09:50:57 UTC
+# Last sync: 2026-09-12 10:35:30 UTC
