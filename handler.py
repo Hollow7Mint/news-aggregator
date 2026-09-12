ec316b9886439904a9049b85761bfe5ef6a8a1fa
James Holloway <james.holloway94@gmail.com>
2026-09-12 08:42:59 +0000

    finish mongodb sharding sync heartbeat after balance

--- handler.py
+++ handler.py
@@ -216,4 +216,4 @@ class NewsHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-12 07:35:11 UTC
+# Last sync: 2026-09-12 08:42:59 UTC
